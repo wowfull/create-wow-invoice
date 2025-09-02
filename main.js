2b114905f6f74fae33bb328159b8980d7c974c8d
dasan2211 <ihiroto@ebihome.net>
2025-09-03 02:29:57 +0900

    Add printNotes property to export PDF url

--- main.js
+++ main.js
@@ -199,20 +199,21 @@ function createPdfOutput() {
   //URLを生成する
   const baseUrl = `https://docs.google.com/spreadsheets/d/${sheetId}/export?gid=${invoiceId}&exportFormat=pdf&format=pdf`;
   const pdfOptions = "&exportFormat=pdf&format=pdf"
-              + "&size=A4" //用紙サイズ (A4)
-              + "&portrait=true"  //用紙の向き true: 縦向き / false: 横向き
-              + "&fitw=true"  //ページ幅を用紙にフィットさせるか true: フィットさせる / false: 原寸大
-              + "&top_margin=0.50" //上の余白
-              + "&right_margin=0.50" //右の余白
-              + "&bottom_margin=0.50" //下の余白
-              + "&left_margin=0.50" //左の余白
-              + "&horizontal_alignment=CENTER" //水平方向の位置
-              + "&vertical_alignment=TOP" //垂直方向の位置
-              + "&printtitle=false" //スプレッドシート名の表示有無
-              + "&sheetnames=false" //シート名の表示有無
-              + "&gridlines=false" //グリッドラインの表示有無
-              + "&fzr=false" //固定行の表示有無
-              + "&fzc=false" //固定列の表示有無;
+    + "&size=A4" //用紙サイズ (A4)
+    + "&portrait=true"  //用紙の向き true: 縦向き / false: 横向き
+    + "&fitw=true"  //ページ幅を用紙にフィットさせるか true: フィットさせる / false: 原寸大
+    + "&top_margin=0.50" //上の余白
+    + "&right_margin=0.50" //右の余白
+    + "&bottom_margin=0.50" //下の余白
+    + "&left_margin=0.50" //左の余白
+    + "&horizontal_alignment=CENTER" //水平方向の位置
+    + "&vertical_alignment=TOP" //垂直方向の位置
+    + "&printtitle=false" //スプレッドシート名の表示有無
+    + "&sheetnames=false" //シート名の表示有無
+    + "&gridlines=false" //グリッドラインの表示有無
+    + "&fzr=false" //固定行の表示有無
+    + "&fzc=false" //固定列の表示有無
+    + "&printnotes=false" //メモの表示有無;
   const url = baseUrl + pdfOptions;
 
   console.log(fileName);
